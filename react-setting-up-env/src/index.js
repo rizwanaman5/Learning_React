ece16b130d1f89c7b2a179278849c1b3432bc6e4
Rizwan Aman <rizwanaman5@gmail.com>
2019-10-31 13:22:44 +0530

    how to set up react without create-react-app

---
+++ react-setting-up-env/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App(){
+    return(
+        <h1> Hello from WebPAck React App</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
---
+++ react-setting-up-env/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+var htmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry : "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+       ]
+    },
+    mode : "production",
+    plugins: [
+        new htmlWebpackPlugin({
+         template: "./public/index.html"
+       })
+    ]
+};
